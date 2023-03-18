f6083bf9e557d909b0ef52841027f4ec5186ea40
Riya <21cs74@ecajmer.ac.in>
2023-03-18 13:02:15 +0530

    background fit and removing inline css from scheme

--- src/pages/scheme.js
+++ src/pages/scheme.js
@@ -1,50 +1,85 @@
 import React from "react";
 
 const Home = () => {
-    return <a href="/"></a>;
-  };
+  return <a href="/"></a>;
+};
 
-  const About = () => {
-    return <a href="/About/"></a>;
-  };
+const About = () => {
+  return <a href="/About/"></a>;
+};
 
 const Scheme = () => {
-    return (
-        <div style={{ backgroundColor:'rgba(114, 138, 172, 1)', padding:'60px',fontFamily: 'Barlow, sans-serif',}}>
-            <div style={{ textAlign:'center',  }}>
-            <h1>SCHEME</h1>
-            <p>STATE SCHEME|NATIONAL SCHEME</p>
-            </div>
-            <div className="d-flex">
-                <div className="flex-row">
-                <div className='col-md-4'>
-                    <h4>Ayushman Bharat</h4>
-                    <p>LOREMLREML OREJHD SSVHSYVHGFUYVB GVHBFVHBFVHJBSFVJ HBHSFVJHFBVJHFBVJVBKJ HVBSFFKVHFI ULKVNDFLIKV NDSFILVNK</p>
-                </div>
-                <div style={{ backgroundColor:'rgba(189, 217, 191, 1)', borderRadius:'38px',padding:'20px'}} className='col-md-4'>
-                    <h4>Ayushman Bharat</h4>
-                    <p>LOREM LREML OREJHD SSVHSYV HGFUYVB GVHBF VHBFVHJB SFVJHBHSFVJ HFBVJHFB VJVBKJ HVBSFFKVH FIULKVNDFL IKVNDS FILVNK</p>
-                </div>
-                <div style={{ backgroundColor:'rgba(189, 217, 191, 1)', borderRadius:'38px',padding:'20px'}} className='col-md-4'>
-                    <h4>Ayushman Bharat</h4>
-                    <p>LOREML REMLORE JHDSSVH SYVHGFUYVB GVHBFV HBFVHJB SFVJHBHS FVJHFBV JHFBVJ VBKJHVB SFFKVH FIULKVND FLIKVN DSFILVNK</p>
-                </div>
-                <div style={{ backgroundColor:'rgba(189, 217, 191, 1)', borderRadius:'38px',padding:'20px'}} className='col-md-4'>
-                    <h4>Ayushman Bharat</h4>
-                    <p>LOREMLR EMLORE JHDSSV HSYVHGFUYVB GVHBF VHBFVH JBSFVJH BHSFVJH FBVJHFBVJ VBKJHVBSF FKVHFIULKV NDFLIKVN DSFILVNK</p>
-                </div>
-                <div style={{ backgroundColor:'rgba(189, 217, 191, 1)', borderRadius:'38px', padding:'20px',}} className='col-md-4'>
-                    <h4>Ayushman Bharat</h4>
-                    <p>LOREML REMLOREJH DSSVHSYV HGFUYVB GVHBFVHBF VHJBSFVJ HBHSFVJH FBVJHFBV JVBKJHV BSFFKVHF IULKVNDF LIKVNDSF ILVNK</p>
-                </div>
-                <div style={{ backgroundColor:'rgba(189, 217, 191, 1)', borderRadius:'38px', padding:'20px'}} className='col-md-4'>
-                    <h4>Ayushman Bharat</h4>
-                    <p>LOREM LREMLO REJHDSS VHSYVHGFUYVB GVHBFVH BFVHJBSFVJ HBHSFVJHFBV JHFBVJVBKJH VBSFFKVHFI ULKVNDFL IKVNDS FILVNK</p>
-                </div>
-                </div>
-            </div>
+  return (
+    <div className="scheme-page">
+      <div className="scheme-titles">
+        <h1 className="scheme">SCHEME</h1>
+        <p className="state-scheme">STATE SCHEME|NATIONAL SCHEME</p>
+      </div>
+      <div className="d-flex" style={{}}>
+        <div className="row" style={{padding:'10px'}}>
+          <div
+            
+            className="col-md-4 col-sm-6 scheme-box"
+          >
+            <h1>Ayushman Bharat</h1>
+            <p>
+              LOREMLREML OREJHD SSVHSYVHGFUYVB GVHBFVHBFVHJBSFVJ
+              HBHSFVJHFBVJHFBVJVBKJ HVBSFFKVHFI ULKVNDFLIKV NDSFILVNK
+            </p>
+          </div>
+          <div
+            
+            className="col-md-4 col-sm-6 scheme-box"
+          >
+            <h1>Ayushman Bharat</h1>
+            <p>
+              LOREM LREML OREJHD SSVHSYV HGFUYVB GVHBF VHBFVHJB SFVJHBHSFVJ
+              HFBVJHFB VJVBKJ HVBSFFKVH FIULKVNDFL IKVNDS FILVNK
+            </p>
+          </div>
+          <div
+            
+            className="col-md-4 col-sm-6 scheme-box"
+          >
+            <h1>Ayushman Bharat</h1>
+            <p>
+              LOREML REMLORE JHDSSVH SYVHGFUYVB GVHBFV HBFVHJB SFVJHBHS FVJHFBV
+              JHFBVJ VBKJHVB SFFKVH FIULKVND FLIKVN DSFILVNK
+            </p>
+          </div>
+          <div
+            
+            className="col-md-4 col-sm-6 scheme-box"
+          >
+            <h1>Ayushman Bharat</h1>
+            <p>
+              LOREMLR EMLORE JHDSSV HSYVHGFUYVB GVHBF VHBFVH JBSFVJH BHSFVJH
+              FBVJHFBVJ VBKJHVBSF FKVHFIULKV NDFLIKVN DSFILVNK
+            </p>
+          </div>
+          <div
+            
+            className="col-md-4 col-sm-6 scheme-box"
+          >
+            <h1>Ayushman Bharat</h1>
+            <p>
+              LOREML REMLOREJH DSSVHSYV HGFUYVB GVHBFVHBF VHJBSFVJ HBHSFVJH
+              FBVJHFBV JVBKJHV BSFFKVHF IULKVNDF LIKVNDSF ILVNK
+            </p>
+          </div>
+          <div
+            className="col-md-4 col-sm-6 scheme-box"
+          >
+            <h1>Ayushman Bharat</h1>
+            <p>
+              LOREM LREMLO REJHDSS VHSYVHGFUYVB GVHBFVH BFVHJBSFVJ HBHSFVJHFBV
+              JHFBVJVBKJH VBSFFKVHFI ULKVNDFL IKVNDS FILVNK
+            </p>
+          </div>
         </div>
-    );
-    }
+      </div>
+    </div>
+  );
+};
 
 export default Scheme;
